9a593a8020d62b4a7dfc9257108fa825c5894bbc
AmrishCM <Amrishmuruganathan@gmail.com>
2025-02-12 18:42:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,6 @@ function handleNoClick() {
 function handleYesClick() {
     window.location.href = "yes_page.html";
 }
+function handleinstaClick() {
+    window.location.href = "https://www.instagram.com/_i._.amrish/";
+}
